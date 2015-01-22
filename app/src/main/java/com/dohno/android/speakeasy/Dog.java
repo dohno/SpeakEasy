f9cc828cd8c364fd085a703cf2cee2362952a8ee
dohno <email@dohno.com>
2015-01-21 23:21:57 -0500

    Initial commit

---
+++ app/src/main/java/com/dohno/android/speakeasy/Dog.java
@@ -0,0 +1,10 @@
+package com.dohno.android.speakeasy;
+
+/**
+ * Created by dohno on 1/21/2015.
+ */
+public class Dog implements Speaker {
+    public String speak() {
+        return "woof";
+    }
+}
---
+++ app/src/main/java/com/dohno/android/speakeasy/Speaker.java
@@ -0,0 +1,8 @@
+package com.dohno.android.speakeasy;
+
+/**
+ * Created by dohno on 1/21/2015.
+ */
+public interface Speaker {
+    public String speak();
+}
